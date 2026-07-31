602da2f0567f9fd5dc55dd7c03c906d971ab8a8e
Diego Hernan Piola <gestiontr2025@gmail.com>
2026-07-31 11:50:01 -0300

    version 0.14

--- version.py
+++ version.py
@@ -35,7 +35,7 @@ rutas ni otras configuraciones.
 # VERSIÓN ACTUAL DEL PROYECTO
 # ==========================================================
 
-PROJECT_VERSION = "0.13"
+PROJECT_VERSION = "0.14"
 
 
 # ==========================================================
